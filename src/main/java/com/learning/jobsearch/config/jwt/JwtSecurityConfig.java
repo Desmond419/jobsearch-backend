7e5e471277b975591624f3f7855af4a6a2ec19f2
Desmond <7547461@gmail.com>
2022-08-11 22:21:56 +0800

    :bug: Fixed cors error

--- src/main/java/com/learning/jobsearch/config/jwt/JwtSecurityConfig.java
+++ src/main/java/com/learning/jobsearch/config/jwt/JwtSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -36,5 +37,12 @@ public class JwtSecurityConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(tokenInterceptor).addPathPatterns("/**").excludePathPatterns(excludePath);
         WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000/", "https://jobsearch-frontend-mxzqkztmf-learning-project.vercel.app/")
+                .allowedMethods("*");
     }
 }
